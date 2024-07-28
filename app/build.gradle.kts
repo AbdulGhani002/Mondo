ccae3dd8903c4585a7578cad70fe0c8b072e1a7c
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2024-07-28 08:33:07 +0500

    Translations are added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("org.jsoup:jsoup:1.16.1")
 }
--- app/src/main/java/com/project/mondo/models/TranslationResponse.java
+++ app/src/main/java/com/project/mondo/models/TranslationResponse.java
@@ -3,156 +3,47 @@ package com.project.mondo.models;
 import java.util.List;
 
 public class TranslationResponse {
-    private String result;
-    private String pronunciation;
-    private String fromPronunciation;
-    private Definitions definitions;
-    private List<String> examples;
-    private List<Translation> translations;
-    private List<ReversedTranslation> reversedTranslations;
-    private String frequency;
+    private Translations translations;
 
-
-
-    public String getResult() {
-        return result;
-    }
-
-    public void setResult(String result) {
-        this.result = result;
-    }
-
-    public String getPronunciation() {
-        return pronunciation;
-    }
-
-    public void setPronunciation(String pronunciation) {
-        this.pronunciation = pronunciation;
-    }
-
-    public String getFromPronunciation() {
-        return fromPronunciation;
-    }
-
-    public void setFromPronunciation(String fromPronunciation) {
-        this.fromPronunciation = fromPronunciation;
-    }
-
-    public Definitions getDefinitions() {
-        return definitions;
-    }
-
-    public void setDefinitions(Definitions definitions) {
-        this.definitions = definitions;
-    }
-
-    public List<String> getExamples() {
-        return examples;
-    }
-
-    public void setExamples(List<String> examples) {
-        this.examples = examples;
-    }
-
-    public List<Translation> getTranslations() {
+    public Translations getTranslations() {
         return translations;
     }
 
-    public void setTranslations(List<Translation> translations) {
+    public void setTranslations(Translations translations) {
         this.translations = translations;
     }
 
-    public List<ReversedTranslation> getReversedTranslations() {
-        return reversedTranslations;
-    }
+    public static class Translations {
+        private List<TranslationItem> noun;
 
-    public void setReversedTranslations(List<ReversedTranslation> reversedTranslations) {
-        this.reversedTranslations = reversedTranslations;
-    }
-
-    public String getFrequency() {
-        return frequency;
-    }
-
-    public void setFrequency(String frequency) {
-        this.frequency = frequency;
-    }
-
-    public static class Translation {
-        private String translation;
-
-        public String getTranslation() {
-            return translation;
-        }
-
-        public void setTranslation(String translation) {
-            this.translation = translation;
-        }
-    }
-
-    public static class ReversedTranslation {
-        private String translation;
-
-        public String getTranslation() {
-            return translation;
-        }
-
-        public void setTranslation(String translation) {
-            this.translation = translation;
-        }
-    }
-
-    public static class Definitions {
-        private List<Definition> exclamation;
-        private List<Definition> noun;
-        private List<Definition> verb;
-
-
-        public List<Definition> getExclamation() {
-            return exclamation;
-        }
-
-        public void setExclamation(List<Definition> exclamation) {
-            this.exclamation = exclamation;
-        }
-
-        public List<Definition> getNoun() {
+        public List<TranslationItem> getNoun() {
             return noun;
         }
 
-        public void setNoun(List<Definition> noun) {
+        public void setNoun(List<TranslationItem> noun) {
             this.noun = noun;
         }
 
-        public List<Definition> getVerb() {
-            return verb;
+        private List<TranslationItem> adjective;
+
+        public List<TranslationItem> getAdjective() {
+            return adjective;
         }
 
-        public void setVerb(List<Definition> verb) {
-            this.verb = verb;
+        public void setAdjective(List<TranslationItem> adjective) {
+            this.adjective = adjective;
         }
     }
 
-    public static class Definition {
-        private String definition;
-        private String example;
+    public static class TranslationItem {
+        private String translation;
 
-
-
-        public String getDefinition() {
-            return definition;
+        public String getTranslation() {
+            return translation;
         }
 
-        public void setDefinition(String definition) {
-            this.definition = definition;
-        }
-
-        public String getExample() {
-            return example;
-        }
-
-        public void setExample(String example) {
-            this.example = example;
+        public void setTranslation(String translation) {
+            this.translation = translation;
         }
     }
 }
